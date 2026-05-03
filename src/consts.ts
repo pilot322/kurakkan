2b7c3c8c671c936519703033a4fab778707a6387
michalis <michalis.lappas@outlook.com>
2026-05-03 16:12:06 +0300

    chore(config): update site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Kurakkan';
-export const SITE_DESCRIPTION = 'Super mind dump';
+export const SITE_DESCRIPTION = "My personal site";
 export const SITE_AUTHOR = 'Michalis Lappas';
 export const SOCIAL_LINKS = {
 	github: 'https://github.com/pilot322',
